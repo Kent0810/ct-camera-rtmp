ac5de84d1adcac51eae82e6f78600decc38e33a4
Kent0810 <kent.bui207ktmt@hcmut.edu.vn>
2023-09-18 00:27:05 +0700

    fix up streaming stuttering issue

--- open_cv_streaming.py
+++ open_cv_streaming.py
@@ -4,6 +4,11 @@ import cv2
 # def start_ffplay(rtmp_url):
 #     return sp.Popen(['ffplay', '-listen', '1', '-i', rtmp_url])
 
+# This is the XML location + file containing the pre-trained classifier for detecting frontal faces in images
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+
+
 def start_ffmpeg(rtmp_url, width, height, fps):
     command = [
         'ffmpeg',
@@ -17,8 +22,8 @@ def start_ffmpeg(rtmp_url, width, height, fps):
         '-i', '-',
         '-c:v', 'libx264',
         '-pix_fmt', 'yuv420p',
-        '-preset', 'ultrafast',
-        '-bufsize', '64M',
+        '-preset', 'fast',
+        '-bufsize', '256M',
         '-f', 'flv',
         rtmp_url
     ]
@@ -26,14 +31,13 @@ def start_ffmpeg(rtmp_url, width, height, fps):
 
 def face_detection(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minSize=(30, 30))
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     return cv2.flip(frame, 1)
 
 def main():
-    rtmp_url = "rtmp://localhost:1935/live/test"  # TODO Dynamic URL, Stream keys...
+    rtmp_url = "rtmp://192.168.1.6:1935/live/test"  # TODO Dynamic URL, Stream keys...
     cap = cv2.VideoCapture(0)
 
     # Get video information
@@ -46,13 +50,9 @@ def main():
 
     # Read web camera
     while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("End of input file")
-            break
-
-
-         # Detect faces
+        _, frame = cap.read()
+      
+        # Detect faces
         frame = face_detection(frame)
 
         # Write to pipe
@@ -64,4 +64,5 @@ def main():
     ffplay_process.kill()  # Forcefully close FFplay sub-process
 
 if __name__ == '__main__':
+        
     main()
