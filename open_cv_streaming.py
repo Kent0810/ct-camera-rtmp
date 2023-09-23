e93da94c351025861f72032fef2df5531abc4380
Kent0810 <kent.bui207ktmt@hcmut.edu.vn>
2023-09-23 19:56:55 +0700

    add bit-rate

--- open_cv_streaming.py
+++ open_cv_streaming.py
@@ -24,6 +24,7 @@ def start_ffmpeg(rtmp_url, width, height, fps):
         '-preset', 'fast',
         '-bufsize', '256M',
         '-f', 'flv',
+        '-b', '4000000',
         rtmp_url
     ]
     return sp.Popen(command, stdin=sp.PIPE)
